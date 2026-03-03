0a63ae2eaa526c67306435bd971d110547a62dc4
Samuel Nduka <ndukas@users.noreply.github.com>
2026-03-03 21:11:06 +0100

    style: decrease splash speed and increase loading time to 2500ms

--- src/components/SplashScreen.tsx
+++ src/components/SplashScreen.tsx
@@ -13,7 +13,7 @@ export default function SplashScreen({ onComplete }: { onComplete: () => void })
       setTimeout(() => {
         onComplete();
       }, 250);
-    }, 1200);
+    }, 2500);
     return () => {
       clearTimeout(timer);
     };
@@ -30,7 +30,7 @@ export default function SplashScreen({ onComplete }: { onComplete: () => void })
         >
           <div className="relative flex flex-col items-center">
             <AppleHelloEnglishEffect
-              speed={2.8}
+              speed={1.2}
               className="h-24 md:h-32 text-white"
             />
           </div>
